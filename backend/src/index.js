d53d5d0ab68e1afbedb4c056d40bf9a412d662d4
LeticiaFarias <leticia_farias@alu.ufc.br>
2020-01-15 14:24:04 -0300

    2 Criando a base da aplicação 1:13:03

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect('mongodb+srv://leticia:OmniStack@cluster0-rey68.mongodb.net/week10?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
+
+app.use(express.json());//precisa vir antes das rotas
+
+app.use(routes);
+
+
+
+//Métodos HTTP: GET, POST, PUT DELETE
+
+// Tipos de parametros: 
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração, remoção)
+// Body: request.body (Dados para alteração, criação de um registro)
+
+//MongoDB (não-relacional)
+
+
+app.listen(3333);
