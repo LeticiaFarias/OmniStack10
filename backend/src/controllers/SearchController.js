d1d752af2bca933364b58aa76e49159904a7a434
LeticiaFarias <leticia_farias@alu.ufc.br>
2020-01-16 21:37:44 -0300

    Controllers

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,11 @@
+const Dev = require('../models/Dev')
+
+module.exports = {
+   async indexe(request, response) {
+      // Busca todos os devs num raio de 10km
+      // Filtrar por tecnologias
+
+      
+
+   }
+}
